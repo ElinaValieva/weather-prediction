b57e48650fb914209740c402f02ad611f42045ed
ElinaValieva <veaufa@mail.ru>
2018-04-16 12:24:50 +0300

    weatherLoader from api.openweathermap for 5 day weather forecast

---
+++ venv/Include/test.py
@@ -0,0 +1,67 @@
+import requests
+import math
+from datetime import datetime, time, date
+
+
+citys = ["oselki", "kipen", "pavlovsk", "peterhof", "lisiy nos", "kronstadt", "kolpino", "vyborg"]
+filenameTest = 'wx_test'
+filenamePrediction = 'wy_test'
+
+# function to download data from api.openweathermap
+def getData(city):
+    request = requests.get("http://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid=45b7a8b65841193a9b57eaf237df1693")
+    data = request.json()
+    day = datetime.now().date().day
+    tmp = data['list']
+    temp = 0
+    pressure = 0
+    clouds = 0
+    deg = 0
+    speed = 0
+    result = []
+    for i in range(0, len(tmp)):
+        if (i % 5 != 0 and i != 0):
+            tempF = tmp[i]['main']['temp']
+            temp = temp + math.ceil((tempF - 273.15) / 1.8)
+            pressure = pressure + tmp[i]['main']['pressure']
+            clouds = (1 if (tmp[i]['clouds']['all'] > 50) else 0) + clouds
+            deg = deg + convertDeg(tmp[i]['wind']['deg'])
+            speed = speed + tmp[i]['wind']['speed']
+        else:
+            temp = math.ceil(temp / 5)
+            pressure = math.ceil(pressure / 5)
+            clouds = math.ceil(clouds /5)
+            deg = math.ceil(deg /5)
+            speed = math.ceil(speed /5)
+            st = str(temp) + ";" + str(pressure) + ";" + str(clouds) + ";" + str(deg) + ";" + str(speed)
+            result.append(st)
+            temp = 0
+            pressure = 0
+            clouds = 0
+            deg = 0
+            speed = 0
+    return result
+
+def convertDeg(deg):
+    if (deg in range(0, 45) or deg == 360):
+        result = 6
+    elif (deg in range (46, 89)):
+        result = 7
+    elif (deg in range(90, 135)):
+        result = 8
+    elif (deg in range(136, 179)):
+        result = 9
+    elif (deg in range(180, 225)):
+        result = 2
+    elif (deg in range(225, 269)):
+        result = 3
+    elif (deg in range(270, 305)):
+        result = 4
+    elif (deg in range(306, 359)):
+        result = 5
+    else :
+        result = 0
+    return result
+
+result = getData(citys[1])
+print(result)
